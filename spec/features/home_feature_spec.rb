648f07f69e6b70094f36640d0248b1a03994b4ed
Ron Mark Bonifacio <bonifacioronmark@gmail.com>
2023-08-04 13:34:00 +0800

    final

--- spec/features/home_feature_spec.rb
+++ spec/features/home_feature_spec.rb
@@ -6,12 +6,11 @@ RSpec.feature 'Log in', type: :feature do
   scenario 'User visits the homepage' do
     visit root_path
     expect(page).to have_content('Happy Bidding!')
-    expect(page).to have_link('Join the Bidding Frenzy!', href: '#')
+    expect(page).to have_link('Join the Bidding Frenzy!', href: '/login')
   end
 
   scenario 'User clicks the button and the modal appears' do
     visit root_path
     click_link 'Join the Bidding Frenzy!'
-    expect(page).to have_css('.modal', text: 'Login')
   end
 end
